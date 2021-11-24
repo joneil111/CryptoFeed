4c42f43941eb2a476487055a7a44b8e32b804656
218002566 <hjmoodley@gmail.com>
2021-11-24 19:23:00 +0200

    removed border tab

--- Screens/TabScreen.js
+++ Screens/TabScreen.js
@@ -1,48 +1,23 @@
 import React from "react";
 import { createMaterialBottomTabNavigator } from "@react-navigation/material-bottom-tabs";
 import AntDesign from "react-native-vector-icons/AntDesign";
-import { createStackNavigator } from '@react-navigation/stack';
 
 import Market from "./Market";
 import News from "./News";
 import Home from "./Home";
 import NewsScreen from "../NewsScreen";
-import DetailsScreen from './DetailsScreen';
 
-const Stack = createStackNavigator();
 const Tab = createMaterialBottomTabNavigator();
-
-function HomeStack() {
-	return (
-		<Stack.Navigator
-			initialRouteName="Home"
-			screenOptions={{
-			headerStyle: { backgroundColor: '#E9F0FB' },
-			headerTintColor: '#5B628F',
-			headerTitleStyle: { fontWeight: 'bold' }
-			}}>
-			<Stack.Screen
-				name="Home"
-				component={Home}
-				options={{ title: 'Home' }}/>
-			<Stack.Screen
-				name="Details"
-				component={DetailsScreen}
-				options={{ title: 'Historic CryptoCurrency' }} />
-		</Stack.Navigator>
-	);
-}
 
 const TabNavigator = (navigation) => (
   <Tab.Navigator
     initialRouteName="HomeScreen"
     activeColor="#fff"
     shifting={true}
-    style={{ borderRadius: 10 }}
   >
     <Tab.Screen
       name="Home"
-      component={HomeStack}
+      component={Home}
       options={{
         tabBarLabel: "Home",
         tabBarColor: "#009387",
